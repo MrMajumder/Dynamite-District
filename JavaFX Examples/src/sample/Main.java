eaeff491b696a7d8990c4daaeeaf90506087b26b
Shafayat Hossain Majumder <40597489+MrMajumder@users.noreply.github.com>
2019-01-11 23:03:22 +0600

    Sprite class uploaded

---
+++ JavaFX Examples/src/sample/Main.java
@@ -0,0 +1,111 @@
+package sample;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class Main extends Application {
+    
+    //creating a stage object as a separate method for referrence purposes
+    public Stage theStage;
+    
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        DoubleValue degree = new DoubleValue();
+        theStage = primaryStage;
+        theStage.setTitle( "Timeline Example" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+        Canvas canvas = new Canvas( 1000, 1000 );
+        root.getChildren().add( canvas );
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        theScene.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                degree.setValue(Math.toDegrees(Math.atan2( event.getY() - canvas.getHeight()/2, event.getX() - canvas.getWidth()/2)));
+            }
+        });
+
+        ArrayList<String> keyInput = new ArrayList<>();
+        theScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e)
+            {
+                String code = e.getCode().toString();
+
+                // only add once... prevent duplicates
+                if ( !keyInput.contains(code) )
+                    keyInput.add( code );
+            }
+        });
+        theScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent e)
+            {
+                String code = e.getCode().toString();
+                keyInput.remove( code );
+            }
+        });
+
+        Sprite characterSprite = new Sprite("images/sprite.png", canvas.getWidth()/2, canvas.getHeight()/2, 5);
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                gc.clearRect(0, 0, 1000, 1000);
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                // background image clears canvas
+                gc.setFill( new Color(1, 1, 1, 1.0) );
+                gc.fillRect(0, 0, 1000, 1000);
+
+                characterSprite.setDegree(degree.getValue());
+
+                if (keyInput.contains("W")) {
+                    characterSprite.goFront(degree.getValue());
+                }
+                if (keyInput.contains("A")) {
+                    characterSprite.goLeft(degree.getValue());
+                }
+                if (keyInput.contains("S")) {
+                    characterSprite.goBack(degree.getValue());
+                }
+                if (keyInput.contains("D")) {
+                    characterSprite.goRight(degree.getValue());
+                }
+
+                characterSprite.render(gc);
+
+            }
+        }.start();
+
+        theStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ JavaFX Examples/src/sample/Sprite.java
@@ -0,0 +1,83 @@
+package sample;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Sprite {
+    private MyImage image;
+    private double positionX;
+    private double positionY;
+    private double velocity;
+    private double width;
+    private double height;
+    private double degree;
+
+    /*Need to add some methods like _ collision detection (creating an invisible rectangle obj of width and height around the sprite), shoot method (this can also be added in a separate class)*/
+
+
+    //constructor - Shaf
+    public Sprite(String imageURL, double positionX, double positionY, double velocity) {
+        this.image = new MyImage();
+        this.image.makeMyImage(imageURL, 150, 150);
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.velocity = velocity;
+        this.width = 150;
+        this.height = 150;
+    }
+
+    //returns the width of the sprite - Shaf
+    public double getWidth() {
+        return width;
+    }
+
+    //returns the height of the sprite - Shaf
+    public double getHeight() {
+        return height;
+    }
+
+    //Sets the degree orientation of the Sprite from x - axis - Shaf
+    public void setDegree(double degree) {
+        this.degree = degree;
+    }
+
+    //Makes the sprite go forward - Shaf
+    public void goFront(double degree)
+    {
+        this.degree = degree;
+        positionX += velocity * Math.cos(Math.toRadians(this.degree));
+        positionY += velocity * Math.sin(Math.toRadians(this.degree));
+    }
+
+    //Makes the sprite go back - shaf
+    public void goBack (double degree)
+    {
+        this.degree = degree;
+        positionX -= velocity * Math.cos(Math.toRadians(this.degree));
+        positionY -= velocity * Math.sin(Math.toRadians(this.degree));
+    }
+
+    //Makes the sprite go left - Shaf
+    public void goLeft (double degree)
+    {
+        this.degree = degree;
+        double tempDegree = degree + 90;
+        //if(degree>)
+        positionX -= velocity * Math.cos(Math.toRadians(tempDegree));
+        positionY -= velocity * Math.sin(Math.toRadians(tempDegree));
+    }
+
+    //Makes the sprite go right - Shaf
+    public void goRight (double degree)
+    {
+        this.degree = degree;
+        double tempDegree = degree + 90;
+        positionX += velocity * Math.cos(Math.toRadians(tempDegree));
+        positionY += velocity * Math.sin(Math.toRadians(tempDegree));
+    }
+
+    //Technically draws the sprite in the canvas - Shaf
+    public void render(GraphicsContext gc)
+    {
+        gc.drawImage( image.setRotate(this.degree), positionX - image.getWidth()/2, positionY - image.getHeight()/2 );
+    }
+}
